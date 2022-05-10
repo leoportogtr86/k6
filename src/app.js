da7a967ce310a12989013cbf91af8ad5fe948818
leoportogtr86 <leoporto_studio@hotmail.com>
2022-05-10 08:22:31 -0300

    add check

--- src/app.js
+++ src/app.js
@@ -14,5 +14,6 @@ export const options = {
 export default function () {
     const res = http.get('https://test.k6.io');
     check(res, { 'status was 20': (r) => r.status == 200 });
+    check(res, { 'verify homepage text': (r) => r.body.includes('Collection of simple web-pages suitable for load testing') })
     sleep(1);
 }
